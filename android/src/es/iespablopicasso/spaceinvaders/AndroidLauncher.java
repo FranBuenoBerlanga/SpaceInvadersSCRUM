5f4d7504a53b40096f1eed19f4698caf9e4a8d34
jimenezsantos <104553513+loresodio@users.noreply.github.com>
2022-11-14 16:53:33 +0100

    Add files via upload

---
+++ android/src/es/iespablopicasso/spaceinvaders/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package es.iespablopicasso.spaceinvaders;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import es.iespablopicasso.spaceinvaders.GdxGameSpaceInvaders;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = true;
+		config.useCompass = true;
+		initialize(new GdxGameSpaceInvaders(), config);
+	}
+}
