12e810d8b89b9f46fbc88549063d260dba3cf89f
jimenezsantos <104553513+loresodio@users.noreply.github.com>
2022-12-02 20:17:30 +0100

    Mejora Gdx.input.justTouched()

--- SpaceInvaders/core/src/com/politecnicomalaga/spaceinvaders/NaveAliada.java
+++ SpaceInvaders/core/src/com/politecnicomalaga/spaceinvaders/NaveAliada.java
@@ -1,5 +1,7 @@
 package com.politecnicomalaga.spaceinvaders;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -24,30 +26,70 @@ public class NaveAliada {
         moverseIzq = false;
         moverseDer = false;
         img = new Texture("NaveAliada.png");
-        velX = 0.5f;
+        velX = 17.0f;
         velY = 0.5f;
+
 
     }
 
     //Pintarse, en un SpriteBatch
-    public void pintarse(SpriteBatch tablero) {
-
-        tablero.draw(img, 100, 100);
-
-    }
-
-    //Disposar
-    public void dispose() {
-
-        if (img != null){
-            img.dispose();
-        }
-
+    public void pintarse( SpriteBatch batch){
+        batch.draw(img, posX, posY);
     }
 
     //Moverse
-    public void moverse(String letra) {
+    public void moverse() {
+        //Donde nos pulsan en la pantalla
+        float pulsacionPosX = Gdx.input.getX();
+        //Me parece inecesario implementar esta variable
+        float pulsacioPosY = Gdx.input.getY();
+
+
+        boolean recienTocado;
+
+        recienTocado = Gdx.input.justTouched();
+
+        //Cuando tocan la pantalla...
+        if (recienTocado) {
+
+
+            //Cuando pulsan la parte izquierda de la pantalla...
+            if (pulsacionPosX <= Gdx.graphics.getWidth() / 2 && posX-velX>0)  {
+                posX -= velX;
+
+
+            //si pulsan la parte derecha...
+                //posX-velX<Gdx.graphics.getWidth()-57 : ni idea del ancho del Texture (57)
+            } else if (pulsacionPosX >= Gdx.graphics.getWidth() / 2 && posX-velX<Gdx.graphics.getWidth()-57) {
+                posX += velX;
+
+            }
+
+        }
+
+
+
+
+
     }
+
+
+    //Control del movimiento de la NaveAmiga mediante el código necesario en
+    //el método render de la clase principal (usando Gdx.input.justTouched(), y
+    //Gdx.input.getX y getY, pensar posibles soluciones)
+    //Control del estado de la pantalla inicial
+    private void controlMovimiento() {
+        //Actualizo el teclado
+        boolean hatocado;
+
+        hatocado = Gdx.input.justTouched();
+
+        if (hatocado) {
+
+
+            }
+        }
+
 
     // estaviva?
 //GETTERS
