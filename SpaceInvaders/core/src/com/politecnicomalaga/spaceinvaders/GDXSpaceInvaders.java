6b9df1e9072d72a47071bd592dea0d97f7980e92
jimenezsantos <104553513+loresodio@users.noreply.github.com>
2022-12-02 20:18:47 +0100

    Mejora  Gdx.input.justTouched()

--- SpaceInvaders/core/src/com/politecnicomalaga/spaceinvaders/GDXSpaceInvaders.java
+++ SpaceInvaders/core/src/com/politecnicomalaga/spaceinvaders/GDXSpaceInvaders.java
@@ -1,30 +1,40 @@
 package com.politecnicomalaga.spaceinvaders;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class GDXSpaceInvaders extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img1, img2, img3;
+	Texture img1, img2, img3,fondo;
 	NaveAliada Aliado;
 	NaveEnemiga Enemigo;
+	//int pasos;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img1 = new Texture("NaveAliada.png");
 		img2 = new Texture("NaveEnemiga.png");
+		fondo = new Texture("fondo.png");
+		Aliado = new NaveAliada(100,50);
+		//Este juego esta a 60 fps, si queremos que los disparos vayan a 30 fps: 60%2==0;
+		//pasos =0;
+
 		//img3 = new Texture("DisparoALiado.png");
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 1, 1);
+		//ScreenUtils.clear(0, 0, 1, 1);
 		batch.begin();
-		batch.draw(img1, 200, 20,64,64);
+		batch.draw(fondo, 0, 0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 		batch.draw(img2, 300, 250,64,64);
+		Aliado.pintarse(batch);
+		Aliado.moverse();
+		//pasos++;
 		//batch.draw(img3, 200, 200);
 		batch.end();
 	}
