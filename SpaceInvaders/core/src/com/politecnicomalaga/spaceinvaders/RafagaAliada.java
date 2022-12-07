bca820c7d23902a084ca12f6fa1aab651b6bc14c
jimenezsantos <104553513+loresodio@users.noreply.github.com>
2022-12-07 18:31:17 +0000

    Clase solo constructor Array rafaga disparoAliado

---
+++ SpaceInvaders/core/src/com/politecnicomalaga/spaceinvaders/RafagaAliada.java
@@ -0,0 +1,14 @@
+package com.politecnicomalaga.spaceinvaders;
+
+
+import java.util.ArrayList;
+
+public class RafagaAliada {
+private ArrayList<DisparoAliado> listaDisparos;
+
+    public RafagaAliada(int alto, Dibujable miDibujoNorma) {
+
+        listaDisparos = new ArrayList<DisparoAliado>();
+
+    }
+}
